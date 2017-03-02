ea2aebebea5f3c54bbc32a1cd9b089863fce716e
abdelrahman mohamed <abdelrahmanmohamed@abdelrahmans-MacBook-Pro.local>
2017-03-02 18:50:05 +0200

    separation and commenting

--- URLcleaning.playground/Contents.swift
+++ URLcleaning.playground/Contents.swift
@@ -3,11 +3,24 @@
 import UIKit
 
 
-
-enum URLsFactory{
+// two cases that we need urls to be build upon
+enum URLsFactory {
     case simpleCall(Paths, page: Int, limit: Int)
     case appSimple(Paths)
     
+}
+
+// api paths
+extension URLsFactory{
+    enum Paths: String {
+        case mainStream = "path"
+        case profileGame = "profileGame"
+        case follow = "follow"
+    }
+}
+
+// url builder
+extension URLsFactory {
     var url: URL {
         switch self {
         case .simpleCall(let path, let page, let limit):
@@ -16,23 +29,17 @@ enum URLsFactory{
             return URL(string: "\(mainDomain)/\(path.rawValue)")!
         }
     }
-    
-    enum Paths: String {
-        case mainStream = "path"
-        case profileGame = "profileGame"
-        case follow = "follow"
-    }
-    
+}
+
+// main webservice url
+extension URLsFactory{
     var mainDomain: String {
         return "www.example.com"
     }
 }
 
-
 let urlWithPaging = URLsFactory.simpleCall(.mainStream, page: 15, limit: 15).url
 let url = URLsFactory.appSimple(.profileGame).url
-
-
 
 
 
